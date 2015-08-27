564b61df2ce4f089d9837e7d6654b6feff7f0004
ZenekeZene <hectorvillarm@gmail.com>
2015-08-27 20:24:34 +0200

    Seccion Contacto estable.

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,11 +9,10 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -21,14 +20,14 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "//formspree.io/hectorvillarm@gmail.com",
+                method: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                  name: name,
+                  email: email,
+                  message: message
                 },
+                dataType: "json",
                 cache: false,
                 success: function() {
                     // Enable button & show success message
@@ -37,7 +36,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tu mensaje ha sido enviado. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -49,7 +48,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Perdón " + firstName + ", mi servidor de correo no esta funcionando bien. Por favor, prueba mas tarde o enviame un email a <i>hectorvillarm@gmail.com</i>!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
