6e7339fa2cc843edfd5a319c983910a45a396a4f
ZenekeZene <hectorvillarm@gmail.com>
2020-04-04 01:32:49 +0200

    Dist

---
+++ social/dist/precache-manifest.27598b9e6c49665e08255b66af4e952f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339e507eae4b2d468f953b1ad47ee934",
+    "url": "/avatar.jpg"
+  },
+  {
+    "revision": "a751165f8b194c94d61470fc6336ba2e",
+    "url": "/avatar2.jpg"
+  },
+  {
+    "revision": "6cc65158de3888b19aa6",
+    "url": "/css/app.1772f04b.css"
+  },
+  {
+    "revision": "5bb431036faa959fc56f24b8f30b0dd4",
+    "url": "/fonts/Portfolio.5bb43103.ttf"
+  },
+  {
+    "revision": "b5e8a3abb58023617044b1890b86c658",
+    "url": "/fonts/Portfolio.b5e8a3ab.woff"
+  },
+  {
+    "revision": "f9374c7c86094d0bb5140866eead9f93",
+    "url": "/fonts/Portfolio.f9374c7c.eot"
+  },
+  {
+    "revision": "ecfbd7e9bda9615cbb832eadc2659ebe",
+    "url": "/img/Portfolio.ecfbd7e9.svg"
+  },
+  {
+    "revision": "bb64b77f7726027442abff9f7b22d84a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc65158de3888b19aa6",
+    "url": "/js/app.3599a4df.js"
+  },
+  {
+    "revision": "05c58f79abd2187608f4",
+    "url": "/js/chunk-vendors.740ec909.js"
+  },
+  {
+    "revision": "6807d751075d2eb3d52aa88df9aae182",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
