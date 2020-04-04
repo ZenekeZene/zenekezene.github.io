70fd5266525858229859dd55bae027eedc1c4a5b
ZenekeZene <hectorvillarm@gmail.com>
2020-04-04 14:18:30 +0200

    Social

---
+++ social/precache-manifest.c4948abd093a80f87de66d227ed0f555.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339e507eae4b2d468f953b1ad47ee934",
+    "url": "/social/avatar.jpg"
+  },
+  {
+    "revision": "a751165f8b194c94d61470fc6336ba2e",
+    "url": "/social/avatar2.jpg"
+  },
+  {
+    "revision": "87eb980481b13453620d",
+    "url": "/social/css/app.5576b07b.css"
+  },
+  {
+    "revision": "2796c00bb7248ed0da150316663ac1f6",
+    "url": "/social/fonts/Portfolio.2796c00b.eot"
+  },
+  {
+    "revision": "27d427f1906c3f2e8f3bd108f7532284",
+    "url": "/social/fonts/Portfolio.27d427f1.ttf"
+  },
+  {
+    "revision": "4fac77cd9e31d90d3f9c569e3c008a7c",
+    "url": "/social/fonts/Portfolio.4fac77cd.woff"
+  },
+  {
+    "revision": "88853eb84e4bd9f3060c22976f328d0f",
+    "url": "/social/img/Portfolio.88853eb8.svg"
+  },
+  {
+    "revision": "a9093a36beffbe474700423927728b98",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "87eb980481b13453620d",
+    "url": "/social/js/app.5472048d.js"
+  },
+  {
+    "revision": "eebc7f516803d6a3d825",
+    "url": "/social/js/chunk-vendors.12d22f10.js"
+  },
+  {
+    "revision": "6807d751075d2eb3d52aa88df9aae182",
+    "url": "/social/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/social/robots.txt"
+  }
+]);
